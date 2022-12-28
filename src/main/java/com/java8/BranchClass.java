5811355469b5fdb32f05f67ff1a809915c33b9fe
Selva <selvakumar.sun@gmail.com>
2022-12-28 09:21:39 -0700

    New method added

--- src/main/java/com/java8/BranchClass.java
+++ src/main/java/com/java8/BranchClass.java
@@ -1,7 +1,18 @@
 package com.java8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BranchClass {
-    public void branch(){
-        System.out.println("Hello Branch");
+    public void addbyMater(){}
+
+      public static void main(String[] args) {
+          Integer jobWorkingTime = 22;
+          int tot = 2;
+          if (jobWorkingTime > 2){
+              System.out.println("Email should send");
+          }else {
+              System.out.println("Email should NOT send");
+          }
     }
 }
