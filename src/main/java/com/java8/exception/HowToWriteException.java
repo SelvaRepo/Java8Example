f7bffc99fa37a886273802d157fbe2561a7f1e6a
Selva <selvakumar.sun@gmail.com>
2023-02-22 13:26:29 -0700

    New class added

--- src/main/java/com/java8/exception/HowToWriteException.java
+++ src/main/java/com/java8/exception/HowToWriteException.java
@@ -8,6 +8,7 @@ public class HowToWriteException {
         List<Integer> integers = Arrays.asList(3, 9, 7, 0, 10, 20);
         integers.forEach(i -> {
             try {
+                System.out.println("---------------Branch");
                 System.out.println(50 / i);
             } catch (ArithmeticException e) {
                 System.err.println(
