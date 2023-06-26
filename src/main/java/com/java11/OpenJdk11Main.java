243e0978e5a8fefe1ddaf4f9a4cb6440ba71a3db
Selva <selvakumar.sun@gmail.com>
2023-06-25 17:41:00 -0700

    Change - branch_0625

--- src/main/java/com/java11/OpenJdk11Main.java
+++ src/main/java/com/java11/OpenJdk11Main.java
@@ -3,6 +3,7 @@ package com.java11;
 public class OpenJdk11Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the main class");
+        System.out.println("Test 1");
         new OpenJdk11Main().run(args);
     }
 
