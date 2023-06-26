238ea809dd8e979aef4ed712a8a606536aea9bca
Selva <selvakumar.sun@gmail.com>
2023-06-25 17:35:05 -0700

    New Main class - branch_0625

---
+++ src/main/java/com/java11/OpenJdk11Main.java
@@ -0,0 +1,12 @@
+package com.java11;
+
+public class OpenJdk11Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the main class");
+        new OpenJdk11Main().run(args);
+    }
+
+    public void run(String[] args) {
+        System.out.println("Hello world from " + getClass().getClassLoader().getName());
+    }
+}
